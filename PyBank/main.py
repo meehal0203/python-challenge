ef90c0f23c240b6f27b853dae2f6aa70e564b070
meehal0203 <146681542+meehal0203@users.noreply.github.com>
2023-11-01 13:58:09 -0700

    vs code

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,10 +3,19 @@ import os
 import csv
 
 #set path to collect data
-my_file = os.path.join("python-challenge","PyBank", "Resources", "budget_data.csv")
+my_file = os.path.join("Resources", "budget_data.csv")
 
-with open(my_file) As csvfile
-    csvreader = csv.reader(csvfile, delimiter=",")
+# open and read csv file
+with open(my_file) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    
+    # read the header row first
+    header = next(csv_reader)
+    print(csv_reader)
+    
+    # read through each row of data after the header
+   # for row in csv_reader:
+        
     
     
 
