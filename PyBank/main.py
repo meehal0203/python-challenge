d0ec15db8e00b2fd766c6e9b1c3f8f0905865f01
meehal0203 <146681542+meehal0203@users.noreply.github.com>
2023-11-02 14:29:00 -0700

    fresh code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,15 @@
+#The total number of months included in the dataset
+
+#The net total amount of "Profit/Losses" over the entire period
+
+#The changes in "Profit/Losses" over the entire period, and then the average of those changes
+
+#The greatest increase in profits (date and amount) over the entire period
+
+#The greatest decrease in profits (date and amount) over the entire period
+
 #imports
+
 import os
 import csv
 
@@ -11,11 +22,26 @@ with open(my_file) as csv_file:
     
     # read the header row first
     header = next(csv_reader)
-    print(csv_reader)
+    print(header)
     
+    #set variables
+    monthly_count = 0
+    total_vol = 0
+    profit_change = 0
+    great_increase = 0
+    great_decrease = 0
+    great_inc_date = " "
+    great_dec_date = " "
+
     # read through each row of data after the header
-   # for row in csv_reader:
-        
+    for row in csv_reader:
+        # find number of months in dataset
+        monthly_count += 1
+       
+        print(monthly_count)
+        # total net amount profit and loss
+        total_vol += int(row[1])
+        print(total_vol)
     
     
 
