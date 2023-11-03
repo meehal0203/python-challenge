4f094b851d396338b8296cb8732c41d7c51d8064
meehal0203 <146681542+meehal0203@users.noreply.github.com>
2023-11-03 16:18:02 -0700

    new code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -17,48 +17,66 @@ import os
 import csv
 
 #set path to collect data
-my_poll = os.path.join("Resources", "election_data.csv")
+CSVPATH = os.path.join("Resources","election_data.csv")
 
-with open(my_poll) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-    
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+# # # declare variables
+total_votes = 0 #total votes
+can_votes = {} #total votes per candidate
+can_options = [] 
+winner_vote = 0 # max amount of votes
+winner_name = " "
+can_name = []
+
+with open(CSVPATH, encoding='UTF-8') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter= ",")
+
+
     # read header row
     header = next(csvreader)
     print(header)
+    
 
-    # # # declare variables
-    total_votes = 0 #total votes
-    can_votes = {} #total votes per candidate
-    can_options = [] #candidate name
-    winner_vote = 0 # max amount of votes
-    winner_name = " "
     
-    
-    #read in data by row
-     # total number of votes cast   
+#read in data by row
+ # total number of votes cast   
     for row in csvreader:
         total_votes += 1
         can_name = row[2]
-    
+    print(total_votes)
+
     # complete list of candidates who received votes
     # find candidates name and add name to list
-        if can_name not in can_options:
-            can_votes[can_name] = 0
-            can_options += [can_name]
-        
+    if can_name not in can_options:
+        can_votes[can_name] = 0
+        can_options += [can_name]
+        # print(can_options)
+
         # tally up votes to each candidate
         can_votes[can_name] += 1
-       #  print(can_votes)
+        # print(can_votes)
         
         # calculate percentage each candidate received
-        percent_votes_each = can_votes / total_votes * 100
-        # print(percent_votes_each)
+        percent_votes_each = (can_votes[can_name]/total_votes) * 100
+        print(percent_votes_each)
 
 
 
         # election winner based on popular vote
-        winner_name = max(can_votes)
+        #if can_votes > winner_vote[0]:
+        #    winner_vote = winner_name[2]
 
+        # list out keys and values separately
+        key_list = list(can_votes.keys())
+        val_list = list(can_votes.values())
+
+        max_votes = max(val_list)
+
+    # print key with val max_votes
+        winner_name = val_list.index(max_votes)
+        max_votes = winner_name
+    # print(key_list[position])
 
         # your final script should both print the analysis to
         #  the terminal and export a text file with the results.
@@ -68,8 +86,8 @@ print("Election Results")
 print("...........................")
 print(f"Total Votes: {total_votes}")
 print("...........................")
-for can_name, can_votes in total_votes.items():
-    print(f"{can_name}: {can_votes:} {percent_votes_each}")
+for can_name, can_votes in can_votes.items():
+    print(f"{can_name:} {can_votes:} {percent_votes_each:.2f}")
 print("...........................")
 print(f"Winner:{winner_name}")
-print("...........................s")
+print("............................")
