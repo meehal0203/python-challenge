540992feea68d18ee2b5dacff4a4e3acd91fd55e
meehal0203 <146681542+meehal0203@users.noreply.github.com>
2023-11-04 13:35:03 -0700

    final with analysis.txt

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -73,7 +73,7 @@ with open(CSVPATH, encoding='UTF-8') as csvfile:
         # your final script should both print the analysis to
         #  the terminal and export a text file with the results.
 
-        # print results
+        # print results to terminal
     print("Election Results")
     print("...........................")
     print(f"Total Votes: {total_votes}")
@@ -85,3 +85,21 @@ with open(CSVPATH, encoding='UTF-8') as csvfile:
     print("...........................")
     print(f"Winner:{winner}")
     print("............................")
+
+# print results to analysis text file
+output_file = open("PyPoll Analysis.txt", "w")
+output_file.write("Election Results\n")
+output_file.write("...........................\n")
+output_file.write(f"Total Votes: {total_votes}\n")
+output_file.write("...........................\n")
+   # calc and write %
+for can_name, votes in can_votes.items():
+    percent_votes_each = (votes / total_votes) * 100
+output_file.write(f"{can_name}: {percent_votes_each:.2f}% ({votes})\n") 
+    
+output_file.write("...........................\n")
+output_file.write(f"Winner:{winner}\n")
+output_file.write("............................\n")
+
+# close output file
+output_file.close()
